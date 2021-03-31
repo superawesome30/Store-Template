c9aa8b61c86e5b8887a1aeac97848bcea7fcdbc6
Jake Hamblin <jekeltor44@gmail.com>
2021-03-30 23:40:38 -0400

    Created

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	// Database Information
+	$db_host = 'localhost';
+	$db_user = ''; // Change this line
+	$db_pass = ''; // Change this line
+	$db_name = ''; // Change this line
+	require("resources.php");
+?>
